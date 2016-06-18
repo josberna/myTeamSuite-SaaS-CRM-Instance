fd97ab60de186cd12615d94927cd2ed3a28523c3
munish <munish@vibhutitechnologies.com>
2016-06-18 22:19:23 +0530

    del

--- user_privileges/user_privileges_1.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-
-//This is the access privilege file
-$is_admin=true;
-
-$user_info=array('user_name'=>'admin','is_admin'=>'on','user_password'=>'adpexzg3FUZAk','confirm_password'=>'adpexzg3FUZAk','first_name'=>'','last_name'=>'Administrator','roleid'=>'H2','email1'=>'webmaster@tksadvisory.com','status'=>'Active','activity_view'=>'This Week','lead_view'=>'Today','hour_format'=>'am/pm','end_hour'=>'','start_hour'=>'08:00','title'=>'','phone_work'=>'','department'=>'','phone_mobile'=>'','reports_to_id'=>'','phone_other'=>'','email2'=>'','phone_fax'=>'','secondaryemail'=>'','phone_home'=>'','date_format'=>'yyyy-mm-dd','signature'=>'','description'=>'','address_street'=>'','address_city'=>'London','address_state'=>'','address_postalcode'=>'','address_country'=>'UK','accesskey'=>'','time_zone'=>'UTC','currency_id'=>'1','currency_grouping_pattern'=>'123,456,789','currency_decimal_separator'=>'','currency_grouping_separator'=>'','currency_symbol_placement'=>'$1.0','imagename'=>'','internal_mailer'=>'1','theme'=>'softed','language'=>'en_us','reminder_interval'=>'1 Minute','asterisk_extension'=>'','use_asterisk'=>'0','ccurrency_name'=>'','currency_code'=>'USD','currency_symbol'=>'&#36;','conv_rate'=>'1.000','record_id'=>'','record_module'=>'','currency_name'=>'USA, Dollars','id'=>'1');
-
-?>
